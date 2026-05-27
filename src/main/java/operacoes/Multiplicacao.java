a37b497db0c2ae9eac2d07e77f0abcf278d0a9d6
nicollymazeika <nicollydepaula10@gmail.com>
2026-05-27 13:47:28 -0300

    feat: implementa multiplicacao no sistema

---
+++ src/main/java/operacoes/Multiplicacao.java
@@ -0,0 +1,10 @@
+package operacoes;
+import java.util.Scanner;
+public class Multiplicacao {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x*y);
+    }
+}
